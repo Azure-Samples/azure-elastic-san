03492de4222f819b8f0d83d191bcba4aba58d606
adarshv98 <101731207+adarshv98@users.noreply.github.com>
2023-08-15 16:25:37 -0400

    Multi-connect/disconnect
    Added multi-connect and disconnect scripts for Windows and Linux.

---
+++ PSH (Windows) Multi-Session Connect Scripts/ElasticSanDocScripts0523/connect.ps1
@@ -0,0 +1,125 @@
+﻿Param(
+    [Parameter(Mandatory, 
+    HelpMessage = "Resource group name")]
+    [string]
+    $ResourceGroupName,
+    [Parameter(Mandatory,
+    HelpMessage = "Elastic SAN name")]
+    [string]
+    $ElasticSanName,
+    [Parameter(Mandatory,
+    HelpMessage = "Volume group name")]
+    [string]
+    $VolumeGroupName,
+    [Parameter(Mandatory,
+    HelpMessage = "Volumes to be connected")]
+    [string[]]
+    $VolumeName,
+    [Parameter(HelpMessage = "Number of sessions to be connected for each volume. Default value is 32. Input value should be in range of 1-32.")]
+    [ValidateRange(1,32)]
+    [int]
+    $NumSession
+)
+
+#################### DEFINITION OF VOLUME DATA ########################
+class VolumeData
+{
+    [ValidateNotNullOrEmpty()][string]$VolumeName
+    [ValidateNotNullOrEmpty()][string]$TargetIQN
+    [ValidateNotNullOrEmpty()][string]$TargetHostName
+    [ValidateNotNullOrEmpty()][string]$TargetPort
+    [AllowNull()][Nullable[System.Int32]]$NumSession
+
+    VolumeData($VolumeName, $TargetIQN, $TargetHostName, $TargetPort, $NumSession) {
+       $this.VolumeName = $VolumeName
+       $this.TargetIQN = $TargetIQN
+       $this.TargetHostName = $TargetHostName
+       $this.TargetPort = $TargetPort
+       $this.NumSession = if ($NumSession -eq 0 -or $NumSession -eq $null) {32} Else {$NumSession}
+    }
+}
+
+##################### CHECK DEPENDENCY #################################
+$title    = 'Confirm'
+$choices  = '&Yes to terminate','&No to proceed with rest of the steps'
+$choices = @(
+    [System.Management.Automation.Host.ChoiceDescription]::new("&Yes to terminate", "Yes to terminate")
+    [System.Management.Automation.Host.ChoiceDescription]::new("&No to proceed with rest of the steps", "No to proceed with rest of the steps")
+)
+
+## iSCSI initiator check 
+$iscsiWarning = $false 
+try {
+    $checkResult = Get-Service -Name MSiSCSI -ErrorAction Stop
+} catch {
+    $iscsiWarning = $true 
+}
+if (($checkResult.Status -ne "Running") -or $iscsiWarning) {
+    $question = 'iSCSI initiator is not installed or enabled. It is required for successful execution of this connect script. Do you wish to terminate the script to install it?'
+    $decision = $Host.UI.PromptForChoice($title, $question, $choices, 0)
+    if ($decision -eq 0) {
+        exit
+    }
+}
+
+## Multipath I/O check
+$multipathWarning = $false 
+try {
+    $checkResult = Get-WindowsFeature -Name 'Multipath-IO' -ErrorAction Stop
+} catch {
+    $multipathWarning = $true 
+}
+if (($checkResult.InstallState -ne "Installed") -or $multipathWarning) {
+    $question = 'Multipath I/O is not installed or enabled. It is recommended for multi-session setup. Do you wish to terminate the script to install it?'
+    $decision = $Host.UI.PromptForChoice($title, $question, $choices, 0)
+    if ($decision -eq 0) {
+        exit
+    }
+}
+
+
+##################### GATHER INFORMATION OF INPUT VOLUMES ####################
+# Get volume group resource to fail fast
+$vg = Get-AzElasticSanVolumeGroup -ResourceGroupName $ResourceGroupName -ElasticSanName $ElasticSanName -Name $VolumeGroupName -ErrorAction Stop
+
+$volumesToConnect= New-Object System.Collections.Generic.List[VolumeData]
+$invalidVolumes = New-Object System.Collections.Generic.List[string]
+# Get each volume in the input volume list and extract the required info for connections 
+foreach($volume in $volumeName) {
+    try {
+        $vol = Get-AzElasticSanVolume -ResourceGroupName $ResourceGroupName -ElasticSanName $ElasticSanName -VolumeGroupName $VolumeGroupName -Name $volume -ErrorAction Stop
+        $targetIqn = $vol.StorageTargetIqn
+        $targetHostname = $vol.StorageTargetPortalHostname
+        $targetPort = $vol.StorageTargetPortalPort
+        $volumesToConnect.Add([VolumeData]::new($volume,$targetIqn,$targetHostname, $targetPort, $numSession))
+        Write-Host Gathered info of $volume successfully -ForegroundColor Cyan
+    } catch {
+        Write-Error $_
+        $invalidVolumes.Add($volume)
+    }    
+}if ($invalidVolumes.Count -gt 0) {
+    # Terminate the script if any of the input volumes are invalid
+    Write-Error "Invalid volumes: $($invalidVolumes -Join ",")" -ErrorAction Stop
+}
+
+############################### CONNECT VOLUMES ############################
+$sessions = Get-IscsiSession
+if ($sessions -ne $null) {
+    $sessions = (Get-IscsiSession).TargetNodeAddress.ToLower() | Select -Unique
+}
+
+foreach($volume in $volumesToConnect) {
+    # Check if the volume is already connected 
+    if ($sessions -ne $null -and $sessions.Contains($volume.TargetIQN.ToLower())) {
+        Write-Host $volume.VolumeName [$($volume.TargetIQN)]: Skipped as this volume is already connected -ForegroundColor Magenta
+        continue
+    }
+    # connect volume 
+    Write-Host $volume.VolumeName [$($volume.TargetIQN)]: Connecting to this volume -ForegroundColor Cyan
+    iscsicli AddTarget $volume.TargetIQN * $volume.TargetHostName $volume.TargetPort * 0 * * * * * * * * * 0
+    $LoginOptions = '0x00000002'
+    for ($i = 0; $i -lt $volume.NumSession; $i++) {
+        iscsicli PersistentLoginTarget $volume.TargetIQN.ToLower() t $volume.TargetHostname.ToLower() $volume.TargetPort Root\ISCSIPRT\0000_0 -1 * $LoginOptions * * * * * * * * * 0
+        iscsicli LoginTarget $volume.TargetIQN t $volume.TargetHostName $volume.TargetPort Root\ISCSIPRT\0000_0 -1 * $LoginOptions * * * * * * * * * 0
+    }
+}
---
+++ PSH (Windows) Multi-Session Connect Scripts/ElasticSanDocScripts0523/disconnect.ps1
@@ -0,0 +1,96 @@
+﻿Param(
+    [Parameter(Mandatory,
+    HelpMessage = "Resource group name")]
+    [string]
+    $ResourceGroupName,
+    [Parameter(Mandatory,
+    HelpMessage = "Elastic SAN name")]
+    [string]
+    $ElasticSanName,
+    [Parameter(Mandatory,
+    HelpMessage = "Volume group name")]
+    [string]
+    $VolumeGroupName,
+    [Parameter(Mandatory,
+    HelpMessage = "Volumes to be disconnected")]
+    [string[]]
+    $VolumeName
+)
+
+$title    = 'Confirm'
+$choices = @(
+    [System.Management.Automation.Host.ChoiceDescription]::new("&Yes", "Yes")
+    [System.Management.Automation.Host.ChoiceDescription]::new("&No", "No")
+)
+$question = 'Running this script will remove access to all the selected volumes, all existing sessions to these volumes will be disconnected. Do you wish to continue?'
+$decision = $Host.UI.PromptForChoice($title, $question, $choices, 0)
+
+if ($decision -eq 1) {
+    Exit
+}
+
+################ Definition of VolumeData #################################
+class VolumeData
+{
+    [ValidateNotNullOrEmpty()][string]$VolumeName
+    [ValidateNotNullOrEmpty()][string]$TargetIQN
+    [ValidateNotNullOrEmpty()][string]$TargetHostName
+    [ValidateNotNullOrEmpty()][string]$TargetPort
+
+    VolumeData($VolumeName, $TargetIQN, $TargetHostName, $TargetPort) {
+       $this.VolumeName = $VolumeName
+       $this.TargetIQN = $TargetIQN
+       $this.TargetHostName = $TargetHostName
+       $this.TargetPort = $TargetPort
+    }
+}
+
+############### Gather information of input volumes ########################
+
+# Get volume group resource to fail fast
+$vg = Get-AzElasticSanVolumeGroup -ResourceGroupName $ResourceGroupName -ElasticSanName $ElasticSanName -Name $VolumeGroupName -ErrorAction Stop
+
+$volumesToDisconnect= New-Object System.Collections.Generic.List[VolumeData]
+$invalidVolumes = New-Object System.Collections.Generic.List[string]
+foreach($volume in $volumeName) {
+    try {
+        $vol = Get-AzElasticSanVolume -ResourceGroupName $ResourceGroupName -ElasticSanName $ElasticSanName -VolumeGroupName $VolumeGroupName -Name $volume -ErrorAction Stop
+        $targetIqn = $vol.StorageTargetIqn
+        $targetHostname = $vol.StorageTargetPortalHostname
+        $targetPort = $vol.StorageTargetPortalPort
+        $volumesToDisconnect.Add([VolumeData]::new($volume,$targetIqn,$targetHostname, $targetPort))
+        Write-Host Gathered info of $volume successfully -ForegroundColor Cyan
+    } catch {
+        Write-Error $_
+        $invalidVolumes.Add($volume)
+    }    
+}
+if ($invalidVolumes.Count -gt 0) {
+    # Terminate the script if any of the input volumes are invalid
+    Write-Error "Invalid volumes: $($invalidVolumes -Join ",")" -ErrorAction Stop
+}
+
+############## Disconnect volumes ###############################
+$persistentTargets = iscsicli ListPersistentTargets
+foreach($volume in $volumesToDisconnect) {
+    $sessions = Get-IscsiSession | ?{$_.TargetNodeAddress -eq $volume.TargetIQN}
+    if ($sessions -eq $null) {
+        Write-Host $volume.VolumeName [$($volume.TargetIQN)]: Skipped as this volume is not connected -ForegroundColor Magenta
+        continue  
+    }
+
+    Write-Host $volume.VolumeName [$($volume.TargetIQN)]: Disconncting volume -ForegroundColor Cyan
+    # remove connected sessions 
+    foreach ($session in $sessions) {
+        iscsicli LogoutTarget $session.SessionIdentifier
+    }
+    # remove persistent targets 
+    # An example of the string of target name --  Target Name           : iqn.2023-02.net.windows.core.blob.elasticsan.es-ldjjtvaggga0:yifantestvol2
+    $persistentTargetCount = (($persistentTargets | ?{$_.ToLower().Contains($volume.TargetIQN.ToLower())}) | ?{($_ -split ":",2)[1].Trim().ToLower() -eq $volume.TargetIQN.ToLower()}).Count
+    for ($i = 0; $i -lt $persistentTargetCount; $i++) {
+        iscsicli RemovePersistentTarget ROOT\ISCSIPRT\0000_0 $volume.TargetIQN -1 $volume.TargetHostName $volume.TargetPort
+    }
+    # remove target
+    iscsicli RemoveTarget $volume.TargetIQN.ToLower()
+
+}
